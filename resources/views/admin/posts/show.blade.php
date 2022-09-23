53547d258bbb4e4aa3da543c29fd0e5f712daf9a
bigeppo <carlog91cast@gmail.com>
2022-09-23 20:57:44 +0200

    stamp tags in show

--- resources/views/admin/posts/show.blade.php
+++ resources/views/admin/posts/show.blade.php
@@ -9,6 +9,13 @@
             <div class="card-body">
                 <h5 class="card-title">{{ $post->title }}</h5>
                 <h6 lass="card-title">{{ $post->author }}</h6>
+                <h6 class="card-title">Tags:
+                    @if (isset($post->tags))
+                        @foreach ($post->tags as $tag)
+                            {{ $tag->name }}
+                        @endforeach
+                    @endif
+                </h6>
                 <p class="card-text">{{ $post->post_content }}</p>
                 <p class="card-text">{{ $post->post_date }}</p>
 
